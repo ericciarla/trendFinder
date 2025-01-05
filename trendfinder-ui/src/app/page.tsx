f804e867a5e3c3c4ae9de3d265f1b661bd1edd42
Caleb Peffer <44934913+calebpeffer@users.noreply.github.com>
2025-01-04 18:13:00 -0800

    Caleb: got basic UI working

---
+++ trendfinder-ui/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-4">Hello Scraper UI</h1>
+      <Button>Click me!</Button>
+    </main>
+  )
+}
